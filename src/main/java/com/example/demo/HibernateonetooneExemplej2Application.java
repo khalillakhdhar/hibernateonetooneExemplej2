bc6d770ba074cc7c1e78009ef9af96de4c2a683a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 14:18:55 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/HibernateonetooneExemplej2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateonetooneExemplej2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateonetooneExemplej2Application.class, args);
+	}
+
+}
